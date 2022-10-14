fe229ec8a6c642310a735a717521850860f0f291
Ulrich Rosenbaum <ulrich.rosenbaum@1und1.de>
2022-10-14 13:23:58 +0200

    Multidimensional array

---
+++ src/MainWalker.java
@@ -0,0 +1,8 @@
+import maze.RoomMaker;
+
+public class MainWalker {
+    public static void main(String[] args) {
+        System.out.println("This is the main Walker class.");
+        RoomMaker.roomMaker();
+    }
+}
---
+++ src/maze/RoomMaker.java
@@ -0,0 +1,23 @@
+package maze;
+
+import java.util.Arrays;
+
+public class RoomMaker {
+    public static void roomMaker() {
+        System.out.println("The maze will be created here.");
+
+        int[][] board = new int[3][3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = i + j; } } // now let's print a two dimensional array in Java
+        for (int[] a : board) {
+            for (int i : a) {
+                System.out.print(i + "\t"); }
+            System.out.println("\n"); } // printing 2D array using Arrays.deepToString() method System.out.println("another way to print 2D arrays");
+        System.out.println(Arrays.deepToString(board));
+
+
+
+
+        }
+    }
